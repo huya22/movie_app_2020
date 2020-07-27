0b6f9a12bf43a35f9daacf740f4ffd617bde5c33
최성훈 <kkyaleuleug@kkyaleuleug.local>
2020-07-27 22:23:34 +0900

    05-4 영화 앱 만들기 워밍업

--- src/App.js
+++ src/App.js
@@ -1,47 +1,19 @@
-//클래스형 컴포넌트를 사용하는 이유 = state를 사용하기 위해 
-
 import React from 'react';
-class App extends React.Component{       
-                                         //실제로는 React.Component 클래스는 다음과 같이 500줄이 넘는 코드로 여러 기능이 구현 되어 있음.
-                                         //따라서 상속 받지 않으면 React.Component의 코드를 하나하나 다 쳐 넣어야 함.
-                                        
-  constructor(props){
-    super(props);
-    console.log('hello');
-  }
-  state = {                              //state는 객체 형태의 데이터
-    count: 0,
+class App extends React.Component{
+  state = {
+    isLoading: true,
+    movies: [],
   };
-  add = () => {
-    this.setState(current => ({
-      count: current.count + 1,
-    }));
+  componentDidMount(){                       //첫번째 인자로 setTimeout(...)을 전달했고
+    //영화 데이터 로딩!
+    setTimeout(() => {              
+      this.setState({isLoading:false});      //state를 바꾸려면 setState() 함수 사용!!
+    }, 6000);                                //두번째 인자로 6000밀리초를 전달
   }
-  minus = () => {
-    this.setState(current => ({
-      count: current.count - 1,
-    }));
+
+  render(){
+    const {isLoading} = this.state;        
+    return <div>{isLoading ? 'Loading...': 'We are ready'}</div>;    //삼항 연산자(true면 왼쪽 flase면 오른쪽 출력)
   }
-  componentDidMount(){
-    console.log('component rendered');
-  }
-  componentDidUpdate(){
-    console.log('I just updated');
-  }
-  componentWillUnmount(){
-    console.log('component rendered');
-  }
-  render(){                                //App 컴포넌트는 클래스(함수가 아님)라서 reuturn 문 사용 불가.
-                                           //클래스형 컴포넌트에서는 JSX를 반환하기 위해 render() 함수를 사용.
-    console.log("I'm rendering");
-    return (                               //함수형 컴포넌트는 return 문으로 JSX 반환.
-      <div>
-        <h1>The number is: {this.state.count} </h1>
-        <button onClick={this.add}>Add</button>
-        <button onClick={this.minus}>Minus</button>
-      </div>
-    );
-                                                       //클해스형 컴포넌트는 return() 함수로 JSX를 반환.
-  }
-}                                          
+}                        
 export default App;
