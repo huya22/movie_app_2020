68ce722901b0b2049054388e615e7506fcb91ef9
huya22 <huya22@daum.net>
2020-08-11 10:31:36 +0900

    Updates

---
+++ precache-manifest.056dcac336eb5d9e1b7595612976a89b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99afd3b456cb3181c7f19234fd9dd145",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "967152cacd53019adafd",
+    "url": "/movie_app_2020/static/css/main.d387ca03.chunk.css"
+  },
+  {
+    "revision": "bb38ca0ac903b612bc15",
+    "url": "/movie_app_2020/static/js/2.cdc3c92b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie_app_2020/static/js/2.cdc3c92b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "967152cacd53019adafd",
+    "url": "/movie_app_2020/static/js/main.9103dda0.chunk.js"
+  },
+  {
+    "revision": "6636fcdddd453d60abc3",
+    "url": "/movie_app_2020/static/js/runtime-main.9a083715.js"
+  }
+]);
